2b0259669d3358f52030c42679fa9b3e82d39653
Zachary Fogg <me@zfo.gg>
2021-02-20 00:09:14 +0800

    some commands

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,33 @@ app.message("vibez", async ({ message, say }) => {
   }
 });
 
+app.command('/vibez-help', async ({ command, ack, say, respond }) => {
+  await ack();
+  await respond({
+    text: `Hey homie! Here's some help :).
+    This is the #vibez channel. Here we all post things about how we’re feeling.
+    Type out a list of adjectives and inlcude an image or song link, and send it.
+    I’ll keep track of how you feel over time and you will be able to share that if you like.`,
+    response_type: 'ephemeral',
+  });
+});
+
+app.command('/vibez-self', async ({ command, ack, say, respond }) => {
+  await ack();
+  await respond({
+    text: `{this will be your radar chart}`,
+    response_type: 'ephemeral',
+  });
+});
+
+app.command('/vibez-channel', async ({ command, ack, say, respond }) => {
+  await ack();
+  await respond({
+    text: `{this will be the channel's radar chart}`,
+    response_type: 'ephemeral',
+  });
+});
+
 async function main() {
   // Start the app
   await app.start(process.env.PORT ? parseInt(process.env.PORT) : 3000);
