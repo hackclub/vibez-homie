6ef8b17a215b34caac79ef775203733e41cf2018
Zachary Fogg <me@zfo.gg>
2021-03-19 12:34:31 -0400

    reply in thread (untested lol)

--- src/messages/vibez.ts
+++ src/messages/vibez.ts
@@ -1,10 +1,16 @@
+import { GenericMessageEvent } from '@slack/bolt'
 import app from '../server'
 
 
 // Listens to incoming messages that contain 'vibez'
 app.message(/vibez/i, async ({ message, say, logger }) => {
   logger.info('❗ vibe echo ❗')
-  if ('user' in message) { // Not all messages have a user
-    await say(`vibez <@${message.user}>.. vibez ✌🏾😌🤘🏾`)
+  if ('user' in message) { // Not all messages have a user FIXME
+    const _message = message as GenericMessageEvent
+    const thread_ts = _message.thread_ts || _message.ts
+    await say({
+      text: `vibez <@${message.user}>.. vibez ✌🏾😌🤘🏾`,
+      thread_ts
+    })
   }
 })
